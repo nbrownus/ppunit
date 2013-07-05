d5b6316d95164f8980edeea2efec733311d1ec86
Nathan Brown <nbrown.us@gmail.com>
2013-07-05 14:40:02 -0700

    Fixing gitignore and moving run states to Test

--- test/Constants.test.js
+++ test/Constants.test.js
@@ -8,12 +8,6 @@ describe('Constants', function () {
         assert.equal(Constants.EXCLUSIVITY.GLOBAL, 2, 'EXCLUSIVITY.GLOBAL constant changed')
     })
 
-    it('Should have constant run states', function () {
-        assert.equal(Constants.RUN_STATE.WAITING, 0, 'RUN_STATE.WAITING constant changed')
-        assert.equal(Constants.RUN_STATE.RUNNING, 1, 'RUN_STATE.RUNNING constant changed')
-        assert.equal(Constants.RUN_STATE.COMPLETED, 2, 'RUN_STATE.COMPLETED constant changed')
-    })
-
     it('Should have constant results', function () {
         assert.equal(Constants.RESULT.SUCCESS, 1, 'Constants.RESULT.SUCCESS constant changed')
         assert.equal(Constants.RESULT.FAILURE, 2, 'Constants.RESULT.FAILURE constant changed')
--- test/Test.test.js
+++ test/Test.test.js
@@ -2,12 +2,21 @@ var Test = require('../lib/Test')
   , assert = require('assert')
 
 describe('Test', function () {
-    it('Should have constant test types', function () {
-        assert.equal(Test.TYPE.NORMAL, 'normal', 'Test.TYPE.NORMAL constant changed')
-        assert.equal(Test.TYPE.BEFORE_EACH, 'beforeEach', 'Test.TYPE.BEFORE_EACH constant changed')
-        assert.equal(Test.TYPE.AFTER_EACH, 'afterEach', 'Test.TYPE.AFTER_EACH constant changed')
-        assert.equal(Test.TYPE.BEFORE_ALL, 'beforeAll', 'Test.TYPE.BEFORE_ALL constant changed')
-        assert.equal(Test.TYPE.AFTER_ALL, 'afterAll', 'Test.TYPE.AFTER_ALL constant changed')
-        assert.equal(Test.TYPE.ROOT, 'root', 'Test.TYPE.ROOT constant changed')
+
+    describe('Constants', function () {
+        it('Should have constant test types', function () {
+            assert.equal(Test.TYPE.NORMAL, 'normal', 'Test.TYPE.NORMAL constant changed')
+            assert.equal(Test.TYPE.BEFORE_EACH, 'beforeEach', 'Test.TYPE.BEFORE_EACH constant changed')
+            assert.equal(Test.TYPE.AFTER_EACH, 'afterEach', 'Test.TYPE.AFTER_EACH constant changed')
+            assert.equal(Test.TYPE.BEFORE_ALL, 'beforeAll', 'Test.TYPE.BEFORE_ALL constant changed')
+            assert.equal(Test.TYPE.AFTER_ALL, 'afterAll', 'Test.TYPE.AFTER_ALL constant changed')
+            assert.equal(Test.TYPE.ROOT, 'root', 'Test.TYPE.ROOT constant changed')
+        })
+
+        it('Should have constant run states', function () {
+            assert.equal(Test.STATE.WAITING, 0, 'Test.STATE.WAITING constant changed')
+            assert.equal(Test.STATE.RUNNING, 1, 'Test.STATE.RUNNING constant changed')
+            assert.equal(Test.STATE.COMPLETED, 2, 'Test.STATE.COMPLETED constant changed')
+        })
     })
 })
