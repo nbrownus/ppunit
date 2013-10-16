d36c12141ecb1af97518fd134261f0fa4f653b98
Nathan Brown <nbrown.us@gmail.com>
2013-10-16 16:02:53 -0700

    Support for failing tests that call done multiple times and better timeout reporting

--- test/Test.test.js
+++ test/Test.test.js
@@ -1,4 +1,7 @@
 var Test = require('../lib/Test')
+  , TestError = require('../lib/TestError')
+  , Suite = require('../lib/Suite')
+  , ms = require('ms')
   , assert = require('assert')
 
 describe('Test', function () {
@@ -34,4 +37,147 @@ describe('Test', function () {
         })
     })
 
+    describe('Completing tests', function () {
+        it('Should note how long a test took if it eventually succeeds after a timeout', function (done) {
+            var test = new Test('testing', function (inner_done) {
+                setTimeout(inner_done, 3)
+            })
+
+            test.timeout(1)
+            test.parent = new Suite()
+            test.on('finish', function (previous) {
+                if (previous) {
+                    try {
+                        assert.equal(
+                            test.error.message
+                            , 'Timeout of 1ms exceeded, eventually succeeded after ' + ms(test.duration)
+                            , 'Test error was incorrect'
+                        )
+
+                        assert(test.error instanceof TestError, 'Error was not a TestError')
+                        assert.equal(previous.result, Test.RESULT.TIMEOUT, 'Previous test result was wrong')
+                        assert.equal(test.result, Test.RESULT.TIMEOUT, 'Current test result was wrong')
+                    } catch (error) {
+                        return done(error)
+                    }
+
+                    done()
+                }
+            })
+
+            test.run()
+        })
+
+        it('Should note how long a test took if it eventually fails after a timeout', function (done) {
+            var test = new Test('testing', function (inner_done) {
+                setTimeout(function () {
+                    inner_done(new Error('failed'))
+                }, 3)
+            })
+
+            test.timeout(1)
+            test.parent = new Suite()
+            test.on('finish', function (previous) {
+                if (previous) {
+                    try {
+                        assert.equal(
+                            test.error.message
+                            , 'Timeout of 1ms exceeded, eventually failed after ' + ms(test.duration)
+                            , 'Test error was incorrect'
+                        )
+
+                        assert(test.error instanceof TestError, 'Error was not a TestError')
+                        assert.equal(test.error.errors[0].message, 'failed', 'TestError did not have the actual error message')
+                        assert.equal(previous.result, Test.RESULT.TIMEOUT, 'Previous test result was wrong')
+                        assert.equal(test.result, Test.RESULT.TIMEOUT, 'Current test result was wrong')
+                    } catch (error) {
+                        return done(error)
+                    }
+
+                    done()
+                }
+            })
+
+            test.run()
+        })
+
+        it('Should fail a successful test if done was called more than once', function (done) {
+            var test = new Test('testing', function (inner_done) {
+                inner_done()
+                inner_done()
+            })
+
+            test.parent = new Suite()
+            test.on('finish', function (previous) {
+                if (previous) {
+                    try {
+                        assert.equal(
+                            test.error.message
+                          , 'done() called multiple times'
+                          , 'Test error was incorrect'
+                        )
+
+                        assert(test.error instanceof TestError, 'Error was not a TestError')
+                        assert.equal(previous.result, Test.RESULT.SUCCESS, 'Previous test result was wrong')
+                        assert.equal(test.result, Test.RESULT.FAILURE, 'Current test result was wrong')
+                    } catch (error) {
+                        return done(error)
+                    }
+
+                    done()
+                }
+            })
+
+            test.run()
+        })
+
+        it('Should ignore mutliple calls to done if the first was a failure', function (done) {
+            var test = new Test('testing', function (inner_done) {
+                inner_done(new Error('failed'))
+                inner_done()
+                setTimeout(function () {
+                    done()
+                }, 10)
+            })
+
+            test.parent = new Suite()
+            test.on('finish', function (previous) {
+                if (previous) {
+                    done(new Error('Should not be here'))
+                }
+            })
+
+            test.run()
+        })
+
+        it('Should fail a successful test if done was called multiple times', function (done) {
+            var test = new Test('testing', function (inner_done) {
+                inner_done()
+                inner_done()
+            })
+
+            test.parent = new Suite()
+            test.on('finish', function (previous) {
+                if (previous) {
+                    try {
+                        assert.equal(
+                            test.error.message
+                          , 'done() called multiple times'
+                          , 'Test error was incorrect'
+                        )
+
+                        assert(test.error instanceof TestError, 'Error was not a TestError')
+                        assert.equal(previous.result, Test.RESULT.SUCCESS, 'Previous test result was wrong')
+                        assert.equal(test.result, Test.RESULT.FAILURE, 'Current test result was wrong')
+                    } catch (error) {
+                        return done(error)
+                    }
+
+                    done()
+                }
+            })
+
+            test.run()
+        })
+    })
 })
